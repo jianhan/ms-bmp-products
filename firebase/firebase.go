a1fde9a3e45bcf29622eef904af8b93d05430ec6
Jian Han <jianhan@mail.com>
2018-06-10 19:32:32 +1000

    [DEV] Finished DB interface and firebase client

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -7,30 +7,30 @@ import (
 	"fmt"
 
 	"firebase.google.com/go"
-	"firebase.google.com/go/auth"
-	"github.com/jianhan/ms-bmp-products/db"
+	"firebase.google.com/go/db"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
 )
 
 var (
-	c    *auth.Client
+	c    *db.Client
 	once sync.Once
 )
 
-func Repository() db.Database {
+func NewFirebaseClient() *db.Client {
 	once.Do(func() {
 		opt := option.WithCredentialsFile("./admin_sdk.json")
 		app, err := firebase.NewApp(context.Background(), nil, opt)
 		if err != nil {
 			logrus.Fatal(err)
+			panic(fmt.Errorf("can not initialize new firebase app : %v\n", err))
 		}
-		c, err = app.Auth(context.Background())
+		c, err = app.Database(context.Background())
 		if err != nil {
 			logrus.Fatal(err)
 			panic(fmt.Errorf("error getting Auth client: %v\n", err))
 		}
 	})
 
-	return r
+	return c
 }
