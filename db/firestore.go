a4354c54148372903a49a3524b43b41029220d9d
Jian Han <jianhan@mail.com>
2018-06-10 22:36:39 +1000

    [DEV] Added duplication checking

--- db/firestore.go
+++ db/firestore.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"context"
+	"errors"
 
 	"cloud.google.com/go/firestore"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/fatih/structs"
 	"github.com/jianhan/ms-bmp-products/firebase"
 	psuppliers "github.com/jianhan/ms-bmp-products/proto/supplier"
@@ -13,31 +15,63 @@ import (
 
 type firestoreDB struct {
 	client *firestore.Client
+	path   string
 }
 
 func NewFirestoreDB(ctx context.Context) Database {
 	return &firestoreDB{
 		client: firebase.NewFirestoreClient(ctx),
+		path:   "suppliers",
 	}
 }
 
 func (f *firestoreDB) UpsertSuppliers(ctx context.Context, suppliers []*psuppliers.Supplier) error {
+	// get all first
+	existingSuppliers, err := f.getAll(ctx)
+	if err != nil {
+		return err
+	}
+	spew.Dump(existingSuppliers)
 	// setup batch
 	batch := f.client.Batch()
 	for _, s := range suppliers {
-		// new record
-		if s.ID == "" {
-			s.ID = uuid.Must(uuid.NewV4()).String()
+		for _, e := range existingSuppliers {
+			if e.Name == s.Name && s.ID == "" {
+				return errors.New("duplicate name")
+			}
 		}
-		batch.Set(f.client.Collection("suppliers").Doc(s.ID), structs.Map(s), firestore.MergeAll)
+		// auto fill IDs
+		for _, s := range suppliers {
+			if s.ID == "" {
+				s.ID = uuid.Must(uuid.NewV4()).String()
+			}
+		}
+		batch.Set(f.client.Collection(f.path).Doc(s.ID), structs.Map(s), firestore.MergeAll)
 	}
 
 	// Commit the batch.
-	_, err := batch.Commit(ctx)
+	_, err = batch.Commit(ctx)
 	if err != nil {
 		logrus.Errorf("error occur while batch set suppliers @UpsertSuppliers: %s", err.Error())
 		return err
 	}
 
+	return nil
+}
+
+func (f *firestoreDB) getAll(ctx context.Context) (suppliers []*psuppliers.Supplier, err error) {
+	t, _ := f.client.Collection(f.path).Documents(ctx).GetAll()
+	for _, v := range t {
+		var s psuppliers.Supplier
+		if err = v.DataTo(&s); err != nil {
+			return nil, err
+		}
+		suppliers = append(suppliers, &s)
+	}
+
+	return
+}
+
+func validateSuppliers(suppliers []*psuppliers.Supplier) error {
 	return nil
 }
