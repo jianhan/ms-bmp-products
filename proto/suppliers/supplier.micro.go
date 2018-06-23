d1f3562691733b2f07292814e436f3d3fc72dc94
Jian Han <jianhan@mail.com>
2018-06-23 12:17:31 +1000

    [DEV] Update supplier proto

--- proto/suppliers/supplier.micro.go
+++ proto/suppliers/supplier.micro.go
@@ -19,6 +19,7 @@ package suppliers
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
+import _ "github.com/golang/protobuf/ptypes/timestamp"
 
 import (
 	client "github.com/micro/go-micro/client"
